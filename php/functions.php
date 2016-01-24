a7f59f066e8f78e1c6617128f7553e1c6bb7b240
Serge Boyko <boykodev@gmail.com>
2016-01-24 14:06:23 +0200

    Add get_snippet function

---
+++ php/functions.php
@@ -0,0 +1,20 @@
+<?php
+
+// get snippet of text
+function get_snippet( $str, $wordCount = 10 ) {
+  return implode( 
+    '', 
+    array_slice( 
+      preg_split(
+        '/([\s,\.;\?\!]+)/', 
+        $str, 
+        $wordCount*2+1, 
+        PREG_SPLIT_DELIM_CAPTURE
+      ),
+      0,
+      $wordCount*2-1
+    )
+  );
+}
+
+?>
