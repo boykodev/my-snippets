5ef8a6dd82210eece876655777a547b0bd1e2f0f
Serge Boyko <boykodev@gmail.com>
2016-01-24 14:27:04 +0200

    Add preloader

---
+++ misc/preloader/script.js
@@ -0,0 +1,7 @@
+// Paste this code under head tag or in a seperate js file.
+
+// Wait for window load
+$(window).load(function() {
+	// Animate loader off screen
+	$(".se-pre-con").fadeOut("slow");
+});
