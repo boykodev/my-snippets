6d0e23af5cbf65f1cdee55b8178c2307cb3b946e
Serge Boyko <boykodev@gmail.com>
2016-01-24 14:25:53 +0200

    Update JS filesize validation
    Add function parameters

--- javascript/functions.js
+++ javascript/functions.js
@@ -1,8 +1,7 @@
 // filesize validation
-function validateFilesize() {
-    $('#file-input').bind('change', function() {
+function validateFilesize(selector, limit) {
+    $(selector).bind('change', function() {
         var filesize = this.files[0].size / 1024 / 1024;
-        var limit = 2;
         if (filesize > limit) {
         	// do stuff
         }
