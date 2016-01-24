f5e083bed9c04c6bd2af9c0b1d7a6c4388844634
Serge Boyko <boykodev@gmail.com>
2016-01-24 14:32:11 +0200

    Add ACF repeater field loop

--- wordpress/acf.php
+++ wordpress/acf.php
@@ -10,4 +10,15 @@ function my_acf_input_admin_head() {
     </script><?php
 }
 
+// repeater field loop
+if( have_rows('repeater_field_name') ):
+ 	// loop through the rows of data
+    while ( have_rows('repeater_field_name') ) : the_row();
+        // display a sub field value
+        the_sub_field('sub_field_name');
+    endwhile;
+else :
+    // no rows found
+endif;
+
 ?>
