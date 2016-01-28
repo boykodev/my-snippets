db401928763941e5c9d6566a3cab553cd880eb2a
Boykodev <boykodev@gmail.com>
2016-01-28 18:25:38 +0200

    Update functions.php

--- php/functions.php
+++ php/functions.php
@@ -1,20 +1,24 @@
 <?php
 
 // get snippet of text
-function get_snippet( $str, $wordCount = 10 ) {
-  return implode( 
-    '', 
-    array_slice( 
-      preg_split(
-        '/([\s,\.;\?\!]+)/', 
-        $str, 
-        $wordCount*2+1, 
-        PREG_SPLIT_DELIM_CAPTURE
-      ),
-      0,
-      $wordCount*2-1
-    )
-  );
+function get_snippet( $str, $wordCount = 10, $ellipsis = true) {
+    $snippet = implode(
+        '',
+        array_slice(
+            preg_split(
+                '/([\s,\.;\?\!]+)/',
+                $str,
+                $wordCount*2+1,
+                PREG_SPLIT_DELIM_CAPTURE
+            ),
+            0,
+            $wordCount*2-1
+        )
+    );
+    if ($ellipsis && mb_strlen($str) > mb_strlen($snippet)) {
+        $snippet .= '...';
+    }
+    return $snippet;
 }
 
 // prevent echo and get output
