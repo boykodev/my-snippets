207128ea7ebda78626bbac256d8c7d37711e66e8
Boykodev <boykodev@gmail.com>
2016-02-01 22:52:14 +0200

    Update functions.php

--- wordpress/functions.php
+++ wordpress/functions.php
@@ -1,7 +1,15 @@
 <?php
 
 // get a url from get_next_posts_link
-$npl = explode('"', get_next_posts_link());
-$npl_url = $npl[1];
+function get_npl() {
+  $npl = explode('"', get_next_posts_link());
+  return $npl[1];
+}
+
+// add favicon to wp_head
+function favicon_link() {
+  echo '<link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />' . "\n";
+}
+add_action('wp_head', 'favicon_link');
 
 ?>
