531e4fe72ae94a56ea09a5b6849e77414b24eb36
Serge Boyko <boykodev@gmail.com>
2016-02-02 20:19:30 +0200

    Create snippets.js

---
+++ javascript/snippets.js
@@ -0,0 +1,15 @@
+/* JavaScript snippets:
+ * #1 - simulate form submit
+ */
+
+// #1 simulate form submit
+var $form = $('<form>', {
+    action: '/',
+    method: 'post'
+});
+var $input = $('<input>', {
+    type: 'hidden',
+    name: 'name',
+    value: 'value'
+});
+$form.append($input).appendTo('body').submit();
