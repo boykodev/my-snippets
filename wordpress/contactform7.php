e8b3e0b4751bcc1dc441e77912b51d39a7e4156e
Boykodev <boykodev@gmail.com>
2016-02-02 17:30:31 +0200

    Create contactform7.php

---
+++ wordpress/contactform7.php
@@ -0,0 +1,8 @@
+<?php
+
+// list of the most used filetypes
+$prefix = 'filetypes:';
+$default = 'jpg|jpeg|png|gif|pdf|doc|docx|ppt|pptx|odt|avi|ogg|m4a|mov|mp3|mp4|mpg|wav|wmv'
+$extended = 'jpg|jpeg|png|gif|pdf|doc|docx|xlsx|xls|ppt|pptx|odt|ogg|m4a|pln|pla|dgn|dwf|2dl|tif|bmp|txt';
+
+?>
