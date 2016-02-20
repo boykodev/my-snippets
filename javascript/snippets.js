37182b0202e17bb6ca3830f01fd8658931b80e79
Serge Boyko <boykodev@gmail.com>
2016-02-20 13:01:13 +0200

    Update snippets.js

--- javascript/snippets.js
+++ javascript/snippets.js
@@ -2,6 +2,7 @@
  * JavaScript snippets:
  * #1 - simulate form submit
  * #2 - prevent scrolling on append
+ * #3 - get full URL of the background image
  */
 
 // #1 simulate form submit
@@ -21,3 +22,7 @@ var oldHeight = $(document).height();
 var oldScroll = $(window).scrollTop();
 // append something
 $(document).scrollTop(oldScroll + $(document).height() - oldHeight);
+
+// #3 get full URL of the background image
+// http://stackoverflow.com/questions/35521770
+var imageUrl = window.getComputedStyle(document.getElementsByClassName('selector')[0], null).getPropertyValue('background-image').split(/'|"/)[1];
