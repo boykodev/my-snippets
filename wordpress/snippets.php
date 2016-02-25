c198374bbbb5c6e4fcd671eb55cf44fa3c386b46
Serge Boyko <boykodev@gmail.com>
2016-02-25 23:37:04 +0200

    Update snippets.php

--- wordpress/snippets.php
+++ wordpress/snippets.php
@@ -4,6 +4,7 @@
  * WordPress snippets:
  * #1 - passing variables to get_template_part()
  * #2 - set wordpress site url to current domain
+ * #3 - use jQuery dollar
  */
 
 // #1 passing variables to get_template_part()
@@ -14,5 +15,8 @@ $site_url = isset($_SERVER['HTTPS']) ? 'https://' : 'http://';
 $site_url .= $_SERVER['SERVER_NAME'];
 define('WP_SITEURL', $site_url);
 define('WP_HOME', $site_url);
+
+// #3 use jQuery dollar
+var $ = jQuery.noConflict();
 
 ?>
