32a293c2d5ceacaa6f77f4d2724299c1e5159495
Serge Boyko <boykodev@gmail.com>
2016-03-08 20:03:20 +0200

    Update functions.php

--- wordpress/functions.php
+++ wordpress/functions.php
@@ -5,6 +5,7 @@
  * #1 - get a url from get_next_posts_link
  * #2 - add favicon to wp_head
  * #3 - get language slug
+ * #4 - shortcode from array args
  */
 
 // #1 get a url from get_next_posts_link
@@ -22,6 +23,17 @@ add_action('wp_head', 'favicon_link');
 // #3 get current language slug
 function get_locale_slug() {
   return substr(get_locale(), 0, 2);
+}
+
+// #4 build a shortcode from array arguments
+function shortcode_from_array($tag, $args = array()) {
+    $shortcode = "[$tag";
+    foreach ($args as $k => $arg) {
+        $shortcode .= " $k='$arg'";
+    }
+    $shortcode .= ']';
+
+    return $shortcode;
 }
 
 ?>
