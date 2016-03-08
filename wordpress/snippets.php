4244419e8e91691fc4ed1d7e3e7c763fa166e9d4
Serge Boyko <boykodev@gmail.com>
2016-03-08 20:07:59 +0200

    Update snippets.php

--- wordpress/snippets.php
+++ wordpress/snippets.php
@@ -5,6 +5,7 @@
  * #1 - passing variables to get_template_part()
  * #2 - set wordpress site url to current domain
  * #3 - use jQuery dollar
+ * #4 - debug disable (for sure)
  */
 
 // #1 passing variables to get_template_part()
@@ -18,5 +19,13 @@ define('WP_HOME', $site_url);
 
 // #3 use jQuery dollar
 var $ = jQuery.noConflict();
+
+// #4 debug disable (for sure)
+ini_set('log_errors','On');
+ini_set('display_errors','Off');
+ini_set('error_reporting', E_ALL );
+define('WP_DEBUG', false);
+define('WP_DEBUG_LOG', true);
+define('WP_DEBUG_DISPLAY', false);
 
 ?>
