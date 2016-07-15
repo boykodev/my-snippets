9ca6722c8dc16276dc236a3bebae4628af4f4e8c
Serge Boyko <boykodev@gmail.com>
2016-07-15 10:58:39 +0300

    Update snippets.php

--- php/snippets.php
+++ php/snippets.php
@@ -4,6 +4,7 @@
  * PHP snippets:
  * #1 - build POST query using file_get_contents
  * #2 - count script execution time
+ * #3 - implode array last element special case
  */
 
 // #1 build POST query using file_get_contents
@@ -28,5 +29,13 @@ $start = microtime(true);
 # ...
 $time = microtime(true) - $start;
 echo "Time: $time";
+
+// #3 implode array last element special case
+$long_liner = join(' and ', array_filter(array_merge(array(join(', ', array_slice($array, 0, -1))), array_slice($array, -1)), 'strlen'));
+
+$last  = array_slice($array, -1);
+$first = join(', ', array_slice($array, 0, -1));
+$both  = array_filter(array_merge(array($first), $last), 'strlen');
+$result = join(' and ', $both);
 
 ?>
