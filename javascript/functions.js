58e67f9c2331c99b7a8874971bee8e7ba4bb4c86
Serge Boyko <boykodev@gmail.com>
2016-11-03 18:00:36 +0200

    Update functions.js

--- javascript/functions.js
+++ javascript/functions.js
@@ -1,6 +1,7 @@
 /**
  * JavaScript functions:
  * #1 - filesize validation
+ * #2 - hidden element height
  */
 
 // #1 filesize validation
@@ -11,4 +12,24 @@ function validateFilesize(selector, limit) {
         	// do stuff
         }
     });
+}
+
+// #2 get height of the hidden element
+function getActualHeight ($element) {
+    // THANKS: http://stackoverflow.com/questions/2345784/
+
+    // find the closest visible parent and get it's hidden children
+    var visibleParent = $element.closest(':visible').children(),
+        thisHeight;
+
+    // set a temporary class on the hidden parent of the element
+    visibleParent.addClass('temp-show');
+
+    // get the height
+    thisHeight = $element.height();
+
+    // remove the temporary class
+    visibleParent.removeClass('temp-show');
+
+    return thisHeight;
 }
