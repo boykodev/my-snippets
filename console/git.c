50106deccbf6bac8f8c86a6e47e3572a12399a6a
Serge Boyko <boykodev@gmail.com>
2017-12-20 10:39:57 +0200

    Update git.c

--- console/git.c
+++ console/git.c
@@ -2,6 +2,7 @@
  * #1 - remove deleted files
  * #2 - delete branch
  * #3 - chmod insensitive
+ * #4 - edit commit message
  */
 
  // #1 remove all manually deleted files
@@ -13,3 +14,6 @@
  
  // #3 don't register chmod changes to files
  git config core.fileMode false
+
+ // #4 edit last commit message
+ git commit --amend
